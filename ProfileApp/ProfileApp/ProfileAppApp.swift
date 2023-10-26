47ea4f9d18fa10aeabd4ce278bb9728a01d83641
gurjnderSingh <gurjindersingh663@gmail.com>
2023-10-26 22:45:04 +0530

    initial commit

---
+++ ProfileApp/ProfileApp/ProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppApp.swift
+//  ProfileApp
+//
+//  Created by Gurjinder Singh on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
